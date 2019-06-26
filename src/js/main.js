80552703a143c436a3a5b74809b9ce9c6049aa2f
Armon Raphiel <araphiel@gmail.com>
2019-06-26 01:37:38 -0500

    add magic grid

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,19 @@
 import $ from 'jquery'
 import 'bootstrap'
 import 'popper.js'
+import magicGrid from 'magic-grid'
 
 // Add jQuery to global scope
 window.$ = $
 window.jQuery = $
+
+if (document.querySelector('.gallery')) {
+
+    let magic = new magicGrid({
+        container: "#masonry",
+        static: true,
+        animate: true,
+    });
+
+    magic.listen();
+}
