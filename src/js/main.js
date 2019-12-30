6f6165ffe42f95361d91de3f159b5f45baf607cf
Armon Raphiel <araphiel@gmail.com>
2019-12-30 16:42:40 -0600

    add color changing background

--- src/js/main.js
+++ src/js/main.js
@@ -28,3 +28,32 @@ if (document.querySelector('.gallery')) {
     setTimeout(x => magic.positionItems(), 200)
     setTimeout(x => magic.positionItems(), 1000)
 }
+
+// Change colors on scroll
+
+const extraColors = ['--yellow', '--blue', '--pink', '--light']
+const chances = [50, 16, 16, 16]
+function chooseWeighted(items, chances) {
+    var sum = chances.reduce((acc, el) => acc + el, 0);
+    var acc = 0;
+    chances = chances.map(el => (acc = el + acc));
+    var rand = Math.random() * sum;
+    return items[chances.filter(el => el <= rand).length];
+}
+
+
+const targets = document.querySelectorAll('main>section');
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        const { color } = entry.target.dataset        
+        // Are we in viewport?
+        if (entry.intersectionRatio > 0 && color) {
+            document.documentElement.style.setProperty('--secondary-color', `var(--${color})`);
+            // observer.unobserve(entry.target);
+        } else {
+            document.documentElement.style.setProperty('--secondary-color', `var(${chooseWeighted(extraColors, chances)})`);
+        }
+    });
+}, );
+
+targets.forEach(target => observer.observe(target))
